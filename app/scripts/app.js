a676fa973d932fc39c82e6d89f19c265dc387a47
jorishr <joris@jorisr.com>
2019-12-13 02:51:02 +0100

    style work completed for all sections; toggle menu tested

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,8 @@
 const toggle    = document.querySelector('.toggle');
 const nav       = document.querySelector('nav'); 
-
-console.log(toggle)
-console.log(nav)
+const toggleIcons = document.querySelectorAll('header i');
 
 toggle.addEventListener('click', function(){    
     nav.classList.toggle('hidden');
+    toggleIcons.forEach(icon => icon.classList.toggle('hidden'))
 });
