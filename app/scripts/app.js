e07f3f9236705303572283ba7389b4c256b6a20a
jorishr <joris@jorisr.com>
2020-03-04 16:02:35 +0100

    dynamic scroll-indicator added and tested

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,10 @@
+(() => {
 const toggle        = document.querySelector('.toggle');
 const nav           = document.querySelector('nav'); 
 const toggleIcons   = document.querySelectorAll('header i');
 const faders        = document.querySelectorAll('.fade-in');
+const scrollIndicator   = document.querySelector('.scroll-indicator');
+const scrollIndicatorBg = document.querySelector('.scroll-indicator__bg');
 
 //hamburger menu toggle
 toggle.addEventListener('click', function(){    
@@ -33,3 +36,35 @@ const appearOnScroll = new IntersectionObserver(function(
 faders.forEach(fader => {
     appearOnScroll.observe(fader);
 })
+
+//add or remove scrollindicator; fill bg color based on scroll height
+window.addEventListener('scroll', ()=>{
+    let windowHeight = Math.max(
+        document.body.scrollHeight,
+        document.documentElement.scrollHeight,
+        document.body.offsetHeight,
+        document.documentElement.offsetHeight,
+        document.body.clientHeight, 	
+        document.documentElement.clientHeight
+    );
+    let currentScroll = window.pageYOffset;  
+    let scrollHeightPercent = Math.ceil((currentScroll / windowHeight) * 100) + 6; 
+    //console.log(scrollHeightPercent);
+    if(scrollHeightPercent >= 10){
+        scrollIndicator.classList.add('reveal');
+        //throttling the 'filling' of the scrollindicator bg color
+        setTimeout(() => {
+            scrollIndicatorBg.style.height = `${scrollHeightPercent}%`; 
+            //console.log(scrollIndicatorBg.style.height)
+        }, 300);
+    } else {
+        scrollIndicator.classList.remove('reveal');
+    }
+})
+
+//click scroll to top
+scrollIndicator.addEventListener('click', () => {
+    window.scrollTo(0,0);
+})
+
+})();
